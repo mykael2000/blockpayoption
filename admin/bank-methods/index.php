17efa234565311804c423299dbe2b7fff33170c7
Michael Erameh <eramehmichael2000@gmail.com>
2026-01-15 21:56:56 +0100

    upload

--- admin/bank-methods/index.php
+++ admin/bank-methods/index.php
@@ -8,7 +8,7 @@
 //require_once __DIR__ . '/../../includes/auth.php';
 //require_once __DIR__ . '/../../includes/functions.php';
 
-require_auth();
+//require_auth();
 check_session_timeout();
 
 // Get all bank payment methods
