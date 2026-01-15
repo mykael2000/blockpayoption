e39160cafb1e4ac8b4aeea4e646ae58474819b2b
Michael Erameh <eramehmichael2000@gmail.com>
2026-01-15 21:54:00 +0100

    upload

--- admin/bank-methods/index.php
+++ admin/bank-methods/index.php
@@ -6,7 +6,7 @@
 require_once __DIR__ . '/../../includes/config.php';
 require_once __DIR__ . '/../../includes/db.php';
 require_once __DIR__ . '/../../includes/auth.php';
-require_once __DIR__ . '/../../includes/functions.php';
+//require_once __DIR__ . '/../../includes/functions.php';
 
 require_auth();
 check_session_timeout();
