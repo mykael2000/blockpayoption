4005ac09d89d043ac720bf21798f5ca4ff796322
Michael Erameh <eramehmichael2000@gmail.com>
2026-01-15 22:00:58 +0100

    upload

--- admin/bank-methods/index.php
+++ admin/bank-methods/index.php
@@ -3,13 +3,13 @@
  * Bank Payment Methods - List/Index Page
  */
 
-//require_once __DIR__ . '/../../includes/config.php';
-//require_once __DIR__ . '/../../includes/db.php';
-//require_once __DIR__ . '/../../includes/auth.php';
-//require_once __DIR__ . '/../../includes/functions.php';
+require_once __DIR__ . '/../../includes/config.php';
+require_once __DIR__ . '/../../includes/db.php';
+require_once __DIR__ . '/../../includes/auth.php';
+require_once __DIR__ . '/../../includes/functions.php';
 
-//require_auth();
-//check_session_timeout();
+require_auth();
+check_session_timeout();
 
 // Get all bank payment methods
 try {
